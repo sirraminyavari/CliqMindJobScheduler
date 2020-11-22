4121079d3d3c8b8f8ffac08b459a0f4a11a04615
Ramin <sir.raminyavari@gmail.com>
2020-11-22 13:16:56 +0330

    minor changes

--- util/global_settings.js
+++ util/global_settings.js
@@ -1,8 +1,8 @@
 exports.settings = {
     DBURL: "mongodb://localhost:27017/cliqmind_scheduler",
     WebApp: {
-        BaseURL: "http://localhost:20470",
-        Username: "admin",
-        Password: "y4e9fuwyu0fu9"
+        BaseURL: "",
+        Username: "",
+        Password: ""
     }
 };
